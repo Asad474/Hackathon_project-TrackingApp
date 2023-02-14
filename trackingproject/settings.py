aeac72bb9739a11abdf0cad752a47e56bc9471bb
Asad Sarfaraz <103127005+Asad474@users.noreply.github.com>
2023-02-14 14:54:33 +0530

    Update settings.py

--- trackingproject/settings.py
+++ trackingproject/settings.py
@@ -154,5 +154,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'healthcare2103@gmail.com'
-EMAIL_HOST_PASSWORD = 'vyndhhytfobfqisc'
+EMAIL_HOST_USER = 'django.sendmail41@gmail.com'
+EMAIL_HOST_PASSWORD = 'gzlhdpqkjwxdismq'

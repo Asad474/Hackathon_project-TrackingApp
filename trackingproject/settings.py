027e4e057b0b20ca963e3e20d71d71409572f3e1
Asad Sarfaraz <asadsarfraz474@gmail.com>
2023-06-01 22:48:17 +0530

    TrackingApp

--- trackingproject/settings.py
+++ trackingproject/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-k$mwhton-0w)9rlp+6cca7^#dew&(m&x4hyl#0n12#eqs+als@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
